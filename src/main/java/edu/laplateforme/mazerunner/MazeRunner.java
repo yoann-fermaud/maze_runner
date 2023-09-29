1c877544ac03a607dadca7602598c69dceda1af8
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-09-29 12:47:13 +0200

    [ADD] error messages

--- src/main/java/edu/laplateforme/mazerunner/MazeRunner.java
+++ src/main/java/edu/laplateforme/mazerunner/MazeRunner.java
@@ -1,21 +1,39 @@
 package edu.laplateforme.mazerunner;
 
-
-import edu.laplateforme.mazerunner.generator.simple.SimpleImperfectMazeGenerator;
-import edu.laplateforme.mazerunner.generator.simple.SimplePerfectMazeGenerator;
-import edu.laplateforme.mazerunner.maze.Maze;
-import edu.laplateforme.mazerunner.maze.MazePrinter;
+import edu.laplateforme.mazerunner.maze.MazeHandler;
 
 public class MazeRunner {
-    public static void main(String[] args) {
-        Maze maze = generateMaze(10, 10);
-        MazePrinter.printMaze(maze);
-    }
 
-    private static Maze generateMaze(int width, int height) {
-//        SimpleImperfectMazeGenerator simpleImperfectMazeGenerator = new SimpleImperfectMazeGenerator();
-//        return simpleImperfectMazeGenerator.generateMaze(width, height);
-        SimplePerfectMazeGenerator simplePerfectMazeGenerator = new SimplePerfectMazeGenerator();
-        return simplePerfectMazeGenerator.generateMaze(width, height);
+    public static void main(String[] args) {
+        int width, height;
+        String algorithm = args[2].toLowerCase() + args[3].toLowerCase();
+
+        try {
+            width = Integer.parseInt(args[0]);
+            height = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            System.out.println("""
+                    Erreur: Veuillez fournir une largeur et une hauteur valides supérieurs à 5.
+                    Java(1):
+                    java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]
+                    """);
+            return;
+        }
+
+        if (width < 5 || height < 5) {
+            System.out.println("""
+                    Erreur: Veuillez fournir une largeur et une hauteur valides supérieurs à 5.
+                    Java(1):
+                    java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]
+                    """);
+        } else if (args.length != 4) {
+            System.out.println("""
+                    Erreur: Veuillez fournir un type de labyrinthe et une méthode de génération valides
+                    Java(1):
+                    java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]
+                    """);
+        } else {
+            MazeHandler.optionArg(width, height, algorithm);
+        }
     }
 }
